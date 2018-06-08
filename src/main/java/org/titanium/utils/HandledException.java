483e44207042e6bca8ac54a4b9750e0e7122f527
gsanchez89 <gilberto@thecoursekey.com>
2018-06-08 13:13:12 -0500

    Completed

--- src/main/java/org/titanium/utils/HandledException.java
+++ src/main/java/org/titanium/utils/HandledException.java
@@ -1,7 +1,34 @@
 package org.titanium.utils;
 
+import org.openqa.selenium.WebDriver;
+
 public class HandledException extends Exception{
     public static final long serialVersionUID = 700L;
     public static String file;
+
+    public HandledException(){
+
+    }
+
+    public HandledException(String message){
+        super(message);
+        Log.error(message);
+    }
+
+    public HandledException(String message, Exception ex){
+        this(message);
+        Log.error(ex.getMessage());
+    }
+
+    public HandledException(WebDriver driver, String snapsotError, String message) throws HandledException {
+        this(message);
+        file = BaseClass.takeSnapShot(driver, snapsotError);
+        Log.error("Screenshot create on: " + file);
+    }
+
+    public HandledException(WebDriver driver, String snapsotError, String message, Exception ex) throws HandledException {
+        this(driver, snapsotError, message);
+        Log.error(ex.getMessage());
+    }
 
 }
